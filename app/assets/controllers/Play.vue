329082ae7cad71948cc4b4ef8a7e4bd14fbd547e
MarinSH <70581242+MarinSH@users.noreply.github.com>
2023-02-15 03:48:12 +0100

    Add style all offline pages (#15)

--- app/assets/controllers/Play.vue
+++ app/assets/controllers/Play.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="card border-secondary mb-3" style=" margin: auto; width: fit-content;" >
-        <div class="card-body">
-            <h4 class="card-content">🕹️ Envie de 
-                <a href="/room"><button type="button" class="btn btn-info btn-lg">Jouer</button></a>
-            </h4>
+    <div class="card mt-5" style=" margin: auto; width: fit-content;" >
+        <div class="card-body mb-auto p-4">
+            <h4 class="card-title">Rejoindre une patie</h4>
+            <div  class="text-center mt-3">
+
+                <a href="/room">
+                    <button type="button" class="btn btn-info btn-lg m">🕹️ Jouer</button>
+                </a>
+            </div>
         </div>
     </div>
 </template>
--- app/assets/controllers/Subscription.vue
+++ app/assets/controllers/Subscription.vue
@@ -1,34 +1,41 @@
-<template>
-    <div class="card border-secondary mb-3" style=" margin: auto; width: 15%;" >
-        <div class="card-body">
-            <h4 class="card-content">
-                <button type="button" class="btn btn-info btn-lg">Chabasic 🐱‍👓</button>
-            </h4>
-            <div class="card-content">
-                🧸 Devient l'hôte
-            </div>
-            <div class="card-content">
-                🧶 Configure tes salons
-            </div>
-            <div class="card-content">
-                🎈 Et rejoins tes amis
+<template class="container">
+    <div class="row justify-content-md-center mt-5">
+        <div class="col col-md-auto">
+            <div class="card border-info text-white mb-3" style=" margin: auto;" >
+                <h4 class="card-header text-center mt-3">
+                    <button type="button" class="btn btn-info btn-lg">Chabasic 🐱‍👓</button>
+                </h4>
+                <div class="card-body">
+                    <div class="card-content">
+                        🧸 Devient l'hôte
+                    </div>
+                    <div class="card-content">
+                        🧶 Configure tes salons
+                    </div>
+                    <div class="card-content">
+                        🎈 Et rejoins tes amis
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="card border-secondary mb-3" style=" margin: auto; width: 15%;" >
-        <div class="card-body">
-            <h4 class="card-content">
-                <button type="button" class="btn btn-info btn-lg">Chabonné 🐱‍🚀</button>
-            </h4>
-            <div class="card-content">
-                🥏 Propose des idées
+        <div class="col col-md-auto">
+            <div class="card bg-info text-white mb-3" style=" margin: auto;" >
+                <h4 class="card-header text-center mt-3">
+                    <button type="button" class="btn btn-light btn-lg">Chabonné 🐱‍🚀</button>
+                </h4>
+                <div class="card-body">
+                    <div class="card-content">
+                        🥏 Propose des idées
+                    </div>
+                    <div class="card-content">
+                        🧩 Paramètres aux petits oignons
+                    </div>
+                    <div class="card-content">
+                        ✨ Customise ton profil
+                    </div>
+                </div>
             </div>
-            <div class="card-content">
-                🧩 Paramètres aux petits oignons
-            </div>
-            <div class="card-content">
-                ✨ Customise ton profil
-            </div>
+            
         </div>
     </div>
 </template>
